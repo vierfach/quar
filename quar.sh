35f8c9c28ba192d4f927e02a000ebe8b0ab24b1e
drquadriple <40132105+drquadriple@users.noreply.github.com>
2022-03-08 01:44:42 +0200

    Add files via upload

---
+++ quar.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+while getopts S:R: options; do
+        case $options in
+                S) x=$OPTARG
+                if [[ -d ~/.cache/quar/ ]]; then
+                cd ~/.cache/quar/
+                if [[ -d ~/.cache/quar/${x} ]]; then 
+                rm -rf ~/.cache/quar/${x}
+                fi
+                git clone https://aur.archlinux.org/${x}
+                cd ~/.cache/quar/${x}
+                makepkg -si
+                else
+                mkdir ~/.cache/quar/
+                cd ~/.cache/quar/
+                git clone https://aur.archlinux.org/${x}
+                cd ~/.cache/quar/${x}
+                makepkg -si
+                fi
+                echo -n '- Installation Complete.\n'
+                ;;
+                #removal
+                R) y=$OPTARG
+                if [[ -e /usr/bin/sudo ]]; then
+                sudo pacman -R ${y}
+                else
+                doas pacman -R ${y}
+                fi
+                echo -n '- Removal complete.\n'
+                ;;
+        esac
+done
