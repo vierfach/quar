51f8f0ea11195d86fc07ea867125c5dab39ecb25
drquadriple <yo7a.a1@gmail.com>
2022-05-10 19:51:22 +0200

    Replaced GPL-v3 with MIT.

--- quar.sh
+++ quar.sh
@@ -1,18 +1,10 @@
-    # Quar: A Lightweight AUR helper written in bash.
-    # Copyright (C) 2022 drquadriple
+# Copyright © 2022 drquadriple
 
-    # Quar is free software: you can redistribute it and/or modify
-    # it under the terms of the GNU General Public License as published by
-    # the Free Software Foundation, either version 3 of the License, or
-    # (at your option) any later version.
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-    # Quar is distributed in the hope that it will be useful,
-    # but WITHOUT ANY WARRANTY; without even the implied warranty of
-    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    # GNU General Public License for more details.
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-    # You should have received a copy of the GNU General Public License
-    # along with Quar.  If not, see <https://www.gnu.org/licenses/>.
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #! /bin/bash
 while getopts S:R: options; do
@@ -21,7 +13,7 @@ while getopts S:R: options; do
                 S) x=$OPTARG
                 if [[ -d ~/.cache/quar/ ]]; then
                 cd ~/.cache/quar/
-                if [[ -d ~/.cache/quar/${x} ]]; then 
+                if [[ -d ~/.cache/quar/${x} ]]; then
                 rm -rf ~/.cache/quar/${x}
                 fi
                 git clone https://aur.archlinux.org/${x}
